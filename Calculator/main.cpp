805bbbbb456b5052801c0c754325feed023aabc2
zzy19961112 <897488058@qq.com>
2016-05-07 17:59:51 +0800

    code finish

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -43,7 +43,7 @@ int main(int argc,char* argv[])
             if (!flag)
             {
                 opt.output(que);
-                cout << " ";
+                cout << "= ";
             }
 
             opt.putans(stk);
